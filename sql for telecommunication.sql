96757732535adaddef0547209811c8103730df6c
Sharavni hiremath <157801605+SHRAVANI-8668@users.noreply.github.com>
2026-09-03 17:39:01 +0530

    Add files via upload

--- sql for telecommunication.sql
+++ sql for telecommunication.sql
@@ -1,20 +1,20 @@
 create database tele_churn_dashboard;
 
 USE tele_churn_dashboard;
+SELECT DATABASE ();
 
 CREATE TABLE tele_churn_predictions (
     id  int AUTO_INCREMENT  PRIMARY KEY,
+    intl_plan FLOAT,
     intl_calls FLOAT,
     intl_charge FLOAT,
-    day_mins FLOAT,
+   day_mins FLOAT,
     day_charge FLOAT,
-    eve_mins FLOAT,
     eve_charge FLOAT,
     night_mins FLOAT,
     night_charge FLOAT,
     customer_calls FLOAT,
-    intl_plan FLOAT,
-   churn_prediction VARCHAR(50),
+    churn_prediction VARCHAR(50),
    churn_probability float,
     prediction_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
